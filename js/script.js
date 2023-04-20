c070b6cbd68963e11a7ad22faf24006cf7f810e5
larissamato <matoslarissa31@gmail.com>
2023-04-19 22:35:29 -0300

    :adhesive_bandage::lipstick: fix ListenerDOM and change style file

--- js/script.js
+++ js/script.js
@@ -1,27 +1,100 @@
-const formPalpite = document.querySelector('#form-palpite');
-const inputPalpiteBrasil = document.querySelector('#palpite-brasil');
-const inputPalpiteAlemanha = document.querySelector('#palpite-alemanha');
-const btnPalpite = document.querySelector('#btn-palpite');
-const btnReiniciar = document.querySelector('#btn-reiniciar');
-const resultado = document.querySelector('#resultado');
+document.addEventListener("DOMContentLoaded", function() {
+let guesses = 0;
+let maxGuesses = 10;
+let brasilScore = 0;
+let alemanhaScore = 0;
+let result = "";
+let tips = "";
 
-let golsBrasil = 0;
-let golsAlemanha = 0;
-let palpitesRestantes = 10;
+const form = document.getElementById("game-form");
+const brasilInput = document.getElementById("brasil-score");
+const alemanhaInput = document.getElementById("alemanha-score");
+const submitButton = document.getElementById("submit-button");
+const restartButton = document.getElementById("restart-button");
+const resultDisplay = document.getElementById("result");
+const tipsDisplay = document.getElementById("tips");
 
-function atualizarResultado() {
-  if (palpitesRestantes === 0) {
-    resultado.textContent = `Você perdeu. O placar correto era Brasil ${golsBrasil} x ${golsAlemanha} Alemanha.`;
-    inputPalpiteBrasil.disabled = true;
-    inputPalpiteAlemanha.disabled = true;
-    btnPalpite.disabled = true;
-    btnReiniciar.disabled = false;
-  } else {
-    const palpiteBrasil = Number(inputPalpiteBrasil.value);
-    const palpiteAlemanha = Number(inputPalpiteAlemanha.value);
-    if (palpiteBrasil === golsBrasil && palpiteAlemanha === golsAlemanha) {
-      resultado.textContent = 'Parabéns, você acertou o placar!';
-      inputPalpiteBrasil.disabled = true;
+function generateScores() {
+    brasilScore = Math.floor(Math.random() * 11);
+    alemanhaScore = Math.floor(Math.random() * 11);
+}
+
+function restartGame() {
+    guesses = 0;
+    result = "";
+    tips = "";
+    brasilInput.value = "";
+    alemanhaInput.value = "";
+    resultDisplay.textContent = "";
+    tipsDisplay.textContent = "";
+    form.classList.remove("disabled");
+    submitButton.disabled = false;
+    brasilInput.disabled = false;
+    alemanhaInput.disabled = false;
+    submitButton.focus();
+    generateScores();
+}
+
+function checkGuess() {
+  tips = "";
+  guesses++;
+
+  const brasilGuess = parseInt(brasilInput.value);
+  const alemanhaGuess = parseInt(alemanhaInput.value);
+
+  const scores = [brasilScore, alemanhaScore];
+  const guessesArr = [brasilGuess, alemanhaGuess];
+  const teamNames = ["Brasil", "Alemanha"];
+
+  let correctGuesses = 0;
+
+  for (let i = 0; i < scores.length; i++) {
+    const score = scores[i];
+    const guess = guessesArr[i];
+    const teamName = teamNames[i];
+
+    if (guess === score) {
+      tips += `Seu palpite para o placar do ${teamName} está correto. `;
+      correctGuesses++;
+    } else if (guess > score) {
+      tips += `Seu palpite para o placar do ${teamName} é alto. `;
+    } else {
+      tips += `Seu palpite para o placar do ${teamName} é baixo. `;
     }
   }
+
+  if (correctGuesses === 2) {
+    result = "Você acertou!";
+    form.classList.add("disabled");
+    submitButton.disabled = true;
+    brasilInput.disabled = true;
+    alemanhaInput.disabled = true;
+    restartButton.focus();
+  } else if (guesses >= maxGuesses) {
+    result = "Fim de jogo!";
+    form.classList.add("disabled");
+    submitButton.disabled = true;
+    brasilInput.disabled = true;
+    alemanhaInput.disabled = true;
+    restartButton.focus();
+  }
+
+  resultDisplay.textContent = result;
+  tipsDisplay.textContent = tips;
+
+  brasilInput.focus();
+  brasilInput.select();
 }
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    checkGuess();
+});
+
+restartButton.addEventListener("click", function() {
+    restartGame();
+});
+
+generateScores();
+restartButton.click();
+})
